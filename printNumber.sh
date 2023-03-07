6290202e444f7fa7099233b2342e0bdd37804dbb
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-03-07 09:08:39 +0530

    Add: print Selected number save in dictionary

--- printNumber.sh
+++ printNumber.sh
@@ -1,8 +1,12 @@
 echo " Welcome to workshop 2"
 
+declare -A selected_numbers
 for i in {1..10}; do
   if [ $i -eq 1 ] || [ $i -eq 2 ] || [ $i -eq 4 ] || [ $i -eq 7 ] || [ $i -eq 9 ] || [ $i -eq 10 ]; then
-    echo $i
+    selected_numbers[$i]=$i
   fi
 done
+
+echo "${selected_numbers[@]}"
+
 
