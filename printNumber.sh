fd5ee65fdf81d363260ffb9a3a8320e2845f3298
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-03-07 08:59:55 +0530

    Add: print Selected Number

--- printNumber.sh
+++ printNumber.sh
@@ -1,3 +1,8 @@
 echo " Welcome to workshop 2"
 
-for i in {1..10}; do echo $i; done
+for i in {1..10}; do
+  if [ $i -eq 1 ] || [ $i -eq 2 ] || [ $i -eq 4 ] || [ $i -eq 7 ] || [ $i -eq 9 ] || [ $i -eq 10 ]; then
+    echo $i
+  fi
+done
+
